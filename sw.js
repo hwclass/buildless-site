9aec9bebf9ec46e43b74db2a4fcbad9bc93b9539
Kjaer <kayer.halil@gmail.com>
2019-12-30 17:11:04 +0100

    static resources added to service worker

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("buildless-site").then(cache => {
-      return cache.addAll(["/", "./src/buildless-app.js"]);
+      return cache.addAll([
+        "/",
+        "./static/favicon.ico",
+        "./static/hourglass.gif",
+        "./web_modules/preact.js",
+        "./web_modules/htm/preact.js",
+        "./web_modules/preact-fetch.js",
+        "./src/buildless-app.js"
+      ]);
     })
   );
 });
