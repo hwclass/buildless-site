5e730d45d6591305f24d9b6f751973bfa80e697a
Kjaer <kayer.halil@gmail.com>
2019-12-30 16:55:27 +0100

    removed cache entry on service worker

--- sw.js
+++ sw.js
@@ -1,13 +1,7 @@
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("buildless-site").then(cache => {
-      return cache.addAll([
-        "/",
-        "./static/web_modules/preact.js",
-        "./static/web_modules/htm/preact.js",
-        "./static/web_modules/preact-fetch.js",
-        "./src/buildless-app.js"
-      ]);
+      return cache.addAll(["/", "./src/buildless-app.js"]);
     })
   );
 });
