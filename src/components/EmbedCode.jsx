96702941d2937059e29b1d875e963bee69f7dca6
Baris guler <harderworking.class@gmail.com>
2021-02-02 00:01:47 +0100

    Update iframes, preload static sources, set cache headers on vercel

--- src/components/EmbedCode.jsx
+++ src/components/EmbedCode.jsx
@@ -7,8 +7,8 @@ const EmbedCode = () => (
   >
     <iframe
       src="https://glitch.com/embed/#!/embed/buildless-boilerplate?path=src/components/FilmList.js&previewSize=0&sidebarCollapsed=true&attributionHidden=true"
+      loading="lazy"
       title="buildless-boilerplate on Glitch"
-      allow="geolocation; microphone; camera; midi; vr; encrypted-media"
       style="height: 100%; width: 100%; border: 0;"
     ></iframe>
   </div>
