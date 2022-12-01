59d7f64730bc356db17a9603ab4b7342d93cb6e2
hwclass <harderworking.class@gmail.com>
2022-12-01 01:24:21 +0100

    format code

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,18 +2,20 @@ import { h } from "preact";
 
 import Title from "./Title.tsx";
 
-const Header = ({ title }) => (
-  <header>
-    <Title content={title} />
-    <a href="https://fresh.deno.dev">
-      <img
-        width="197"
-        height="37"
-        src="https://fresh.deno.dev/fresh-badge-dark.svg"
-        alt="Made with Fresh"
-      />
-    </a>
-  </header>
-);
+const Header = ({ title }) => {
+  return (
+    <header>
+      <Title content={title} />
+      <a href="https://fresh.deno.dev">
+        <img
+          width="197"
+          height="37"
+          src="https://fresh.deno.dev/fresh-badge-dark.svg"
+          alt="Made with Fresh"
+        />
+      </a>
+    </header>
+  );
+};
 
 export default Header;
