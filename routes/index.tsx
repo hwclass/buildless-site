4c38f6617ea5514e2e98988b4f77792be53a1552
hwclass <harderworking.class@gmail.com>
2022-12-01 18:57:31 +0100

    revert the .json() back

--- routes/index.tsx
+++ routes/index.tsx
@@ -35,7 +35,7 @@ export const handler: Handlers<SectionsData | null> = {
       return ctx.render(null);
     }
     console.log(`handler:baseUrl + SECTIONS_API_ENDPOINT, ${baseUrl + SECTIONS_API_ENDPOINT}`)
-    const data = await JSON.parse(JSON.stringify(response.body));
+    const data = await response.json();
     //TODO: Remove the logs
     console.log(`handler:sections, ${data.sections}`)
     return ctx.render({ sections: data.sections });
