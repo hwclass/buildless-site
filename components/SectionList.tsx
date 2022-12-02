053929c44c2cbfb6fc76c12a156644a77fbdcffa
hwclass <harderworking.class@gmail.com>
2022-12-02 01:07:16 +0100

    add optional chaining for SectionsList

--- components/SectionList.tsx
+++ components/SectionList.tsx
@@ -3,10 +3,10 @@ import { h } from "preact";
 import Section from "./Section.tsx";
 
 const SectionList = ({ data }: { data: { sections: [] } }) => {
-  console.log(`SectionList:data, ${JSON.stringify(data.sections)}`)
+  console.log(`SectionList:data, ${JSON.stringify(data?.sections)}`)
   return (
     <ul style="list-style: none;padding: 0;">
-      {data.sections.map((section) => (
+      {data?.sections.map((section) => (
         <li style="margin-top: 50px;">
           <Section {...section} />
         </li>
