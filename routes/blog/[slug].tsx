b161040451ab9e2be51b1200d910d33382ab82c0
hwclass <harderworking.class@gmail.com>
2022-12-06 23:09:14 +0100

    add new fresh badge, format code

--- routes/blog/[slug].tsx
+++ routes/blog/[slug].tsx
@@ -6,21 +6,31 @@ export default function BlogPostPage(props: PageProps) {
   //TODO: Fetch the specific blog post based on the slug
 
   const blogPost = {
-    slug: 'test-title',
-    title: 'Test Title',
-    content: 'Test content...'
-  }
+    slug: "test-title",
+    title: "Test Title",
+    content: "Test content...",
+  };
 
   return (
     <main>
-      <h1>{slug === blogPost.slug ? blogPost.title : 'We couldn\'t find the article you\'re looking for.'}</h1>
-      <p>{slug === blogPost.slug ? blogPost.content : 'But we have some other articles here for you...'}</p>
-      {typeof slug === 'undefined' ? (
-        <ul>
-          <li>Some other article title here.</li>
-          <li>Another article title here.</li>
-        </ul>
-      ) : null}
+      <h1>
+        {slug === blogPost.slug
+          ? blogPost.title
+          : "We couldn't find the article you're looking for."}
+      </h1>
+      <p>
+        {slug === blogPost.slug
+          ? blogPost.content
+          : "But we have some other articles here for you..."}
+      </p>
+      {typeof slug === "undefined"
+        ? (
+          <ul>
+            <li>Some other article title here.</li>
+            <li>Another article title here.</li>
+          </ul>
+        )
+        : null}
     </main>
   );
 }
